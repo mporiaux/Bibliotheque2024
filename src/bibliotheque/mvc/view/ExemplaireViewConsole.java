42ab86a7e5c3472872297d2835de6884d2da2727
mporiaux <michel.poriaux@condorcet.be>
2024-04-24 09:24:15 +0200

    correction exercices

--- src/bibliotheque/mvc/view/ExemplaireViewConsole.java
+++ src/bibliotheque/mvc/view/ExemplaireViewConsole.java
@@ -1,16 +1,16 @@
 package bibliotheque.mvc.view;
 
-import bibliotheque.metier.Exemplaire;
-import bibliotheque.metier.Mail;
-import bibliotheque.metier.Ouvrage;
-import bibliotheque.metier.Rayon;
+import bibliotheque.metier.*;
 import bibliotheque.mvc.GestionMVC;
 import bibliotheque.mvc.controller.ControllerSpecialExemplaire;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
 
+import static bibliotheque.mvc.GestionMVC.LOCATIONS;
+import static bibliotheque.mvc.GestionMVC.lv;
 import static bibliotheque.utilitaires.Utilitaire.*;
 import static bibliotheque.utilitaires.Utilitaire.affListe;
 
@@ -102,12 +102,24 @@ public class ExemplaireViewConsole extends AbstractView<Exemplaire> {
                 String descr = sc.nextLine();
                 System.out.println("ouvrage : ");
                 List<Ouvrage> lo = GestionMVC.ov.getAll();
-                //TODO présenter les ouvrages par ordre de titre ==> classe anonyme
+                lo.sort(new Comparator<Ouvrage>() {
+                    @Override
+                    public int compare(Ouvrage o1, Ouvrage o2) {
+                        return o1.getTitre().compareTo(o2.getTitre());
+                    }
+                });
+                //TODO présenter les ouvrages par ordre de titre ==> lambda
                 int ch = choixListe(lo);
                 a = new Exemplaire(mat, descr,lo.get(ch-1));
                 System.out.println("rayon");
                 List<Rayon> lr = GestionMVC.rv.getAll();
-                //TODO présenter les rayons par ordre de code ==> classe anonyme
+                lr.sort(new Comparator<Rayon>() {
+                    @Override
+                    public int compare(Rayon o1, Rayon o2) {
+                        return o1.getCodeRayon().compareTo(o2.getCodeRayon());
+                    }
+                });
+                //TODO présenter les rayons par ordre de code ==> lambda
                 ch= choixListe(lr);
                 a.setRayon(lr.get(ch-1));
                 break;
@@ -155,7 +167,15 @@ public class ExemplaireViewConsole extends AbstractView<Exemplaire> {
    }
 
     private void louer(Exemplaire a) {
-        //TODO chosir un lecteur et enregistrer la location dans LOCATIONS
+        List<Lecteur> llec= lv.getAll();
+        llec.sort(new Comparator<Lecteur>() {
+            @Override
+            public int compare(Lecteur o1, Lecteur o2) {
+                return o1.getNumlecteur()-o2.getNumlecteur();
+            }
+        });
+        int ch = choixListe(llec);
+        LOCATIONS.put(a,llec.get(ch-1));
     }
 
 
--- src/bibliotheque/mvc/view/OuvrageViewConsole.java
+++ src/bibliotheque/mvc/view/OuvrageViewConsole.java
@@ -1,16 +1,12 @@
 package bibliotheque.mvc.view;
 
-import bibliotheque.metier.Exemplaire;
-import bibliotheque.metier.Ouvrage;
-import bibliotheque.metier.TypeOuvrage;
+import bibliotheque.metier.*;
 import bibliotheque.mvc.controller.ControllerSpecialOuvrage;
 import bibliotheque.utilitaires.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
+import static bibliotheque.mvc.GestionMVC.av;
 import static bibliotheque.utilitaires.Utilitaire.*;
 
 
@@ -58,7 +54,38 @@ public class OuvrageViewConsole extends AbstractView<Ouvrage> {
 
 
     public void rechercher() {
-        //TODO rechercher ouvrage en demandant type d'ouvrage, puis l'info unique relative à au type recherché
+        TypeOuvrage[] tto = TypeOuvrage.values();
+        List<TypeOuvrage> lto = Arrays.asList(tto);
+        int choix = Utilitaire.choixListe(lto);
+        Ouvrage o=null;
+        switch (choix){
+            case 1 :
+                System.out.print("isbn :");
+                String isbn = sc.nextLine();
+                o=new Livre("",0,null,0,"","",isbn,0, TypeLivre.ROMAN,"");
+                break;
+
+            case 2 :
+                System.out.print("code :");
+                int codecd = lireInt();
+                o=new CD("",0,null,0,"","",codecd,(byte)0, null);
+                break;
+            case 3 :
+                System.out.print("code :");
+                int codedvd = lireInt();
+                o=new DVD("",0,null,0,"","",codedvd, null,(byte)0);
+                break;
+
+        }
+        o=controller.search(o);
+        if(o!=null){
+            affMsg(o.toString());
+        }
+        else {
+            affMsg("ouvrage inconnu");
+        }
+        //TODO réalisé
+
     }
 
 
@@ -80,15 +107,32 @@ public class OuvrageViewConsole extends AbstractView<Ouvrage> {
 
     public void ajouter() {
         TypeOuvrage[] tto = TypeOuvrage.values();
-        List<TypeOuvrage> lto = new ArrayList<>(Arrays.asList(tto));
+        List<TypeOuvrage> lto = Arrays.asList(tto);
         int choix = Utilitaire.choixListe(lto);
-        Ouvrage a = null;
+        Ouvrage o = null;
         List<OuvrageFactory> lof = new ArrayList<>(Arrays.asList(new LivreFactory(),new CDFactory(),new DVDFactory()));
-        a = lof.get(choix-1).create();
-        //TODO affecter un ou plusieurs auteurs
-        //TODO trier les auteurs présentés par ordre de nom et prénom  ==>  classe anonyme
-        //TODO ne pas présenter les auteurs déjà enregistrés pour cet ouvrage
-        controller.add(a);
+        o = lof.get(choix-1).create();
+        List<Auteur> la= av.getAll();
+        la.sort(new Comparator<Auteur>() {
+                    @Override
+                    public int compare(Auteur o1, Auteur o2) {
+                        if (o1.getNom().equals(o2.getNom())) return o1.getPrenom().compareTo(o2.getPrenom());
+                        return o1.getNom().compareTo(o2.getNom());
+                    }
+                });
+        do {
+            Iterator<Auteur> ita = la.iterator();
+            while (ita.hasNext()) {
+                Auteur a = ita.next();
+                if (o.getLauteurs().contains(a)) ita.remove();
+            }
+            int ch = choixListe(la);
+            if (ch == 0) break;
+            o.addAuteur(la.get(ch-1));
+        }while(true);
+
+        //TODO utiliser Lambda
+        controller.add(o);
     }
 
     protected void special() {
